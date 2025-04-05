937bc9ece3d090cedf3ba910b9350ba9faf6521f
Victor Hugo <victorvh06@gmail.com>
2025-04-04 22:03:56 -0300

    fim aula 1

--- app/main.js
+++ app/main.js
@@ -1,13 +1,38 @@
 let livros = [];
 const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 getBuscarLivrosDaApi();
+const livrosSection = document.getElementById("livros");
 
 async function getBuscarLivrosDaApi() {
-    const res = await fetch(endpointDaApi);
-    livros = await res.json();
-    console.table(livros);
+    try {
+        const response = await fetch(endpointDaApi);
+        if (!response.ok) {
+            throw new Error('Erro ao buscar os livros.');
+        }
+        livros = await response.json();
+        console.table(livros);
+        // livros.forEach(livro => {
+        //     console.log(livro.titulo);
+        // });
+        apresentaLivrosNaUI(livros);
+    } catch(error) {
+        console.error(error.message);
+    }
+}
 
-    livros.forEach(livro => {
-        console.log(livro.titulo);
+function apresentaLivrosNaUI(listaDeLivros) {
+    listaDeLivros.forEach(livro => {
+        livrosSection.innerHTML += `
+        <div class="livro">
+        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <h2 class="livro__titulo">${livro.titulo}</h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$ ${livro.preco}</p>
+        <div class="tags">
+            <span class="tag">${livro.categoria}</span>
+        </div>
+        </div>
+        `
     });
 }
+
